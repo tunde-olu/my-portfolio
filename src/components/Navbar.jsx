24ef32f4d81efba16b83d8736c9e40c247ab9afc
tunde-olu <tundeolu24@gmail.com>
2022-12-14 20:09:56 +0100

    nav scroll time reduced

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             </div>
             <ul className="hidden md:flex">
                 {links.map(({ id, link, href }) => (
-                    <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-110 duration-200">{id !== 6 ? <Link to={link} smooth duration={2000} onClick={() => setNav(false)}>{link}</Link> : <a href={href}>{link}</a>}</li>
+                    <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-110 duration-200">{id !== 6 ? <Link to={link} smooth duration={500} onClick={() => setNav(false)}>{link}</Link> : <a href={href}>{link}</a>}</li>
                 ))}
             </ul>
             <button onClick={() => setNav((prev) => !prev)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
